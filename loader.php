11990365b89631b30d10b4f4ea2f6c72e6538066
Michal Mikolas <nanuqcz@gmail.com>
2014-06-08 19:01:50 +0200

    Initial commit.

---
+++ loader.php
@@ -0,0 +1,5 @@
+<?php
+
+define('Shake\Components\FORM_BOOTSTRAP3', __DIR__ . '/form.bootstrap3.latte');
+define('Shake\Components\FORM_BOOTSTRAP3_HORIZONTAL', __DIR__ . '/form.bootstrap3.horizontal.latte');
+define('Shake\Components\FORM_BOOTSTRAP3_INLINE', __DIR__ . '/form.bootstrap3.inline.latte');
