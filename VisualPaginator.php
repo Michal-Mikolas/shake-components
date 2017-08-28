fa2206531d95f81c4ff3f5e2934acc1e2892a9b8
Michal Mikolas <nanuqcz@gmail.com>
2017-08-28 12:22:10 +0200

    Paginator: Added shortcut for ipub/visual-paginator component.

---
+++ VisualPaginator.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Shake\Components;
+
+use Nette\Reflection,
+	Nette\ComponentModel\IContainer;
+
+
+/**
+ * VisualPaginator
+ * shortcut for 3rd party component: ipub/visual-paginator
+ *
+ * @author  Michal Mikoláš <nanuqcz@gmail.com>
+ * @package Shake
+ */
+class VisualPaginator extends \IPub\VisualPaginator\Components\Control
+{
+
+	/**
+	 * @param string|NULL
+	 * @param IContainer|NULL
+	 * @param string|NULL
+	 */
+	public function __construct($templateFile = NULL, IContainer $parent = NULL, $name = NULL)
+	{
+		parent::__construct($templateFile, $parent, $name);
+
+		$this->setTemplateFile('bootstrap.latte');
+	}
+
+}
